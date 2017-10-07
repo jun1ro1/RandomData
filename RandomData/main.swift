1b3be75258efa5b0cd70e46e0ade3c8e74c30c8e
jun1ro1 <jun1ro1@Beethoven.local>
2017-10-07 22:18:11 +0900

    Initial Commit

---
+++ RandomData/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RandomData
+//
+//  Created by OKU Junichirou on 2017/10/07.
+//  Copyright © 2017年 OKU Junichirou. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
